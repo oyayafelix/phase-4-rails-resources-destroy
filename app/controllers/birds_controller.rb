16c0aec21470eacf35ceabe54e185ea284ce5d5d
oyayafelix <oyayafelix@gmail.com>
2022-09-30 20:43:43 +0300

    commit tree d9e434bc558893fd8d62f77b614280311517b2d8

--- app/controllers/birds_controller.rb
+++ app/controllers/birds_controller.rb
@@ -50,4 +50,15 @@ class BirdsController < ApplicationController
     params.permit(:name, :species, :likes)
   end
 
+  #deleting /birds
+  def destroy
+    bird = Bird.find_by(id: params[:id])
+    if bird
+      bird.destroy
+      head :no_content
+    else
+      render json: { error: "Bird not found" }, status: :not_found
+    end
+  end
+
 end
